3ecc669e27a63fd242c9d86d1cc92271d84d0a6f
Zoe Cortez <zoecortez@campus-057-180.ucdavis.edu>
2024-10-03 14:10:25 -0700

    Updated

--- Week2_IntroR.R
+++ Week2_IntroR.R
@@ -1,8 +1,23 @@
-animals <- c("mouse", "rat", "dog", "cat")
-animals[2]
+## Introduction to R: Arithmetic
+## Incomplete Compound
+## Order of Operations
+# Scientific Notation
+## Mathematical Notation
+## Nested Functions
 
-animals[c(2,3)]
+## Objects and assignment
 
+## rm (list == ls())
 
-animals[c(2,3,2,3,2,3,2)]
- 
+##Challenge
+
+elephant1_kg <- 3492
+Elephant2_lb <- 7757
+
+elephant1_lb <- elephant1_kg * 2.2
+
+elephant2_lb <- elephant1_lb
+
+myelephants <- c(elephants1_lb, elephant2_lb)
+
+which(myelephants) == max(myelephants)
