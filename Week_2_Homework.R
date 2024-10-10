89036c01612f4416a3c1718dd59b7ec674b9ada3
Zoe Cortez <zoecortez@campus-054-092.ucdavis.edu>
2024-10-10 16:01:34 -0700

    hw 1 week 2

--- Week_2_Homework.R
+++ Week_2_Homework.R
@@ -10,7 +10,7 @@ prob1 <- hw2[!is.na(hw2)]
 prob1 <-prob1 [prob1 >14 & prob1 <38] ## only displaying numbers between 14 to 38
 
 times3 <- prob1*3 ## Multiply values by 3
-  
+
 plus10 <- times3 + 10 ## Adding 10 to values
 
 plus10[c(TRUE,FALSE)]
