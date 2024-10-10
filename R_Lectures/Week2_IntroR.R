aa4d13094de22d39b16b5d48d52262196ff93a72
Zoe Cortez <zoecortez@campus-054-092.ucdavis.edu>
2024-10-10 15:38:49 -0700

    test

---
+++ R_Lectures/Week2_IntroR.R
@@ -0,0 +1,23 @@
+## Introduction to R: Arithmetic
+## Incomplete Compound
+## Order of Operations
+# Scientific Notation
+## Mathematical Notation
+## Nested Functions
+
+## Objects and assignment
+
+## rm (list == ls())
+
+##Challenge
+
+elephant1_kg <- 3492
+Elephant2_lb <- 7757
+
+elephant1_lb <- elephant1_kg * 2.2
+
+elephant2_lb <- elephant1_lb
+
+myelephants <- c(elephants1_lb, elephant2_lb)
+
+which(myelephants) == max(myelephants)
